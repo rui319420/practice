2d65a180301d08a8b985df7affbfb545849d3cad
Rui <s1F102501866@iniad.org>
2025-11-16 22:10:05 +0900

    feat: fetch error component

--- React-blog/src/Home.js
+++ React-blog/src/Home.js
@@ -2,25 +2,36 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-    {title: 'WWelcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-    {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3},
-  ])
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs);
-  }
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    console.log('useEffect run')
-    console.log(blogs)
-  })
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        if (!res.ok){
+          throw Error('could not fetch the data');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
+      })
+    }, 1000);
+  }, []);
 
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+      { error && <div>{ error }</div> }
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
     </div>
   );
 }
