cfedeb4394adc223a2a7055bc16aa66da4932ebf
Rui <s1F102501866@iniad.org>
2025-12-01 21:58:39 +0900

    feat: console WeekOfDay

---
+++ calendar/app/Header/WeekHeader/WeekHeader.tsx
@@ -0,0 +1,8 @@
+import styles from "./WeekHeader.module.css"
+
+export function WeekHeader() {
+
+  return (
+    <div className={styles.WeekHeader}></div>
+  )
+}
--- calendar/app/MonthGrid/DayCell/Day.tsx
+++ calendar/app/MonthGrid/DayCell/Day.tsx
@@ -1,14 +1,27 @@
 import styles from "./Day.module.css"
+import { getDay } from "date-fns"
 
 type Props = {
   index: number
 }
 
+
+
 export function DayCell({index}: Props) {
   
+  let DayOfWeek = ''
+
+  const today = new Date()
+  if (getDay(today) === 1) {
+    DayOfWeek = styles.Sunday
+    console.log('Monday')
+  }
+
   return (
     <div className={styles.Cell}>
       {index}
+      <div className={DayOfWeek}>
+      </div>
     </div>
   )
 }
