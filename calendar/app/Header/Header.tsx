459ce98c79e1f34417424dfb7da6db1861e044d2
Rui <s1F102501866@iniad.org>
2025-12-01 22:38:28 +0900

    日曜と土曜に色を付けた

--- calendar/app/Header/Header.tsx
+++ calendar/app/Header/Header.tsx
@@ -1,10 +1,14 @@
 import styles from "./Header.module.css"
+import { WeekHeader } from "./WeekHeader/WeekHeader"
 
 export function Header() {
 
   return (
+  <div>
     <div className={styles.Header}>
       2025年12月
     </div>
+    <WeekHeader></WeekHeader>
+  </div>
   )
 }
--- calendar/app/Header/WeekHeader/WeekHeader.tsx
+++ calendar/app/Header/WeekHeader/WeekHeader.tsx
@@ -2,7 +2,19 @@ import styles from "./WeekHeader.module.css"
 
 export function WeekHeader() {
 
+  const days = ["日", "月", "火", "水", "木", "金", "土"];
+
   return (
-    <div className={styles.WeekHeader}></div>
+    <div className={styles.WeekHeader}>
+      {days.map((day, index) => {
+        const sundayStyle = day === "日" ? styles.Sunday : ""
+        const saturdayStyle = day === "土" ? styles.Saturday : ""
+          return (
+          <div key={index} className={`${styles.Day} ${sundayStyle} ${saturdayStyle}`}>
+          {day}
+          </div>
+          )
+      })}
+    </div>
   )
 }
