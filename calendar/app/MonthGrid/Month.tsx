67192f02327399c00e13d203e6d2f894e9c0e1c0
Rui <s1F102501866@iniad.org>
2025-12-01 22:48:29 +0900

    その月の一日が何曜日から始まるのかのロジックを実装

--- calendar/app/MonthGrid/Month.tsx
+++ calendar/app/MonthGrid/Month.tsx
@@ -1,4 +1,4 @@
-import { getDaysInMonth, startOfMonth, endOfMonth } from "date-fns";
+import { getDaysInMonth, startOfMonth, getDay } from "date-fns";
 import { DayCell } from "./DayCell/Day"
 import styles from "./Month.module.css"
 
@@ -7,14 +7,21 @@ export function Month() {
   const today = new Date();
   // 今日の日付を取得
 
-  const daysInMonth = getDaysInMonth(today);
-  // 今月は何日あるか
+  const daysInMonth = getDaysInMonth(today); // 今月は何日あるか
 
+  const firstDayOfMonth = startOfMonth(today); // 今月の一日は何曜日か
+  const startingDayIndex = getDay(firstDayOfMonth);
+
+  const emptySlots = Array(startingDayIndex).fill(null);
+  
   const cells = [...Array(daysInMonth)].map((_, i) => i + 1);
   // ひと月あたりの日にちを配列にする
 
   return (
     <div className={styles.DayBox}>
+      {emptySlots.map((_, index) => (
+        <div key={`empty-${index}`} />
+      ))}
       {cells.map((day, index) => {
         return (
           <DayCell
