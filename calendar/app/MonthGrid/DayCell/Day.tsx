cf4d453de571c5f99da285552aed36559002814c
Rui <s1F102501866@iniad.org>
2025-12-02 21:50:07 +0900

    土曜と日曜の日付に色を付けた

--- calendar/app/MonthGrid/DayCell/Day.tsx
+++ calendar/app/MonthGrid/DayCell/Day.tsx
@@ -10,8 +10,18 @@ export function DayCell({index}: Props) {
   const today = new Date()
   const todayStyle = index === getDate(today) ? styles.Today : ""
 
+  const currentTargetDate = new Date(today.getFullYear(), today.getMonth(), index);
+
+  const dayOfWeek = getDay(currentTargetDate);
+  const isSunday = dayOfWeek === 0;
+  const isSaturday = dayOfWeek === 6;
+
+  const sundayStyle = isSunday ? styles.Sunday : ""
+  const saturdayStyle = isSaturday ? styles.Saturday : ""
+
+
   return (
-    <div className={`${styles.Cell} ${todayStyle}`}>
+    <div className={`${styles.Cell} ${todayStyle} ${sundayStyle} ${saturdayStyle}`}>
       {index}
     </div>
   )
