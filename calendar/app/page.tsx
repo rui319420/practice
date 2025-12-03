94f633cf265c1a957f4c86a658af1f1abbec3c5d
Rui <s1F102501866@iniad.org>
2025-12-03 16:16:02 +0900

    addSchedule関数の作成

--- calendar/app/page.tsx
+++ calendar/app/page.tsx
@@ -3,12 +3,19 @@
 import { useState } from "react";
 import { Month } from "./MonthGrid/Month";
 import { Header } from "./Header/Header";
-import { addMonths, subMonths } from "date-fns";
+import { addMonths, isSameDay, subMonths, format } from "date-fns";
+
+type Schedule = {
+  date: Date;
+  content: string
+}
 
 export default function Home() {
 
   const [currentDate, setCurrentDate] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [schedules, setSchedules] = useState<Schedule[]>([]);
+  const [inputText, setInputText] = useState("");
 
   const handlePrevMonth = () => {
     setCurrentDate(subMonths(currentDate, 1));
@@ -22,6 +29,23 @@ export default function Home() {
     setSelectedDate(date);
     console.log("クリックされた日:", date);
   };
+
+  const addSchedule = () => {
+  
+    if (!selectedDate || !inputText) return;
+
+    const newSchedule: Schedule = {
+      date: selectedDate,
+      content: inputText,
+    }
+
+    setSchedules([...schedules, newSchedule])
+    setInputText("")
+  }
+
+  const filteredSchedules = schedules.filter(schedule =>
+    selectedDate && isSameDay(schedule.date, selectedDate)
+  );
 
   return (
     <div className="container">
