f86e1b0d4facc0c7648c853f88c426d98712fd72
Rui <s1F102501866@iniad.org>
2026-01-14 14:26:46 +0900

    feat: 加法の実装

--- C/sample.c
+++ C/sample.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+
+int add(void);
+
 int main(void)
 {
-  printf("Hello, world");
+  printf("%d\n", 10 + 3);
+  add();
+  return 0;
+}
+
+int add(void)
+{
+  printf("%d + %d = %d\n", 3, 5, 3 + 5);
   return 0;
 }
