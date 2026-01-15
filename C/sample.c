a9f123f0b49d0ee296401aedc772ab4acf2705a9
Rui <s1F102501866@iniad.org>
2026-01-15 22:10:38 +0900

    feat: subtraction

--- C/sample.c
+++ C/sample.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 int add(void);
+int sub(void);
 
 int main(void)
 {
   printf("%d\n", 10 + 3);
   add();
+  sub();
   return 0;
 }
 
 int add(void)
 {
   printf("%d + %d = %d\n", 3, 5, 3 + 5);
+  return 0;
+}
+
+int sub(void)
+{
+  printf("%d - %d = %d\n", 7, 2, 7 - 2);
   return 0;
 }
